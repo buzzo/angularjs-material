197487976f3ec8707b99a0f50d013e01ca2c2424
buzzo <andre@buzzo.com.br>
2015-08-04 09:34:23 +0100

    feat(feat): Add patient edit/remove dialog
    Added patient dialog where you can edit details

--- app/js/modules/patient/patient.js
+++ app/js/modules/patient/patient.js
@@ -1,26 +1,58 @@
 'use strict';
 
+var fs = require('fs');
+var angular = require('angular');
+
 module.exports = function ($scope, $mdDialog) {
 
-    $scope.people = [
-        {name: 'Janet Perkins', tel: '1112212', notes: 'Next appointment: 12th Dec - 8:00 AM'},
-        {name: 'Mary Johnson', tel: '4545454', notes: ''},
-        {name: 'Peter Carlsson', tel: '658789', notes: ''},
-        {name: 'Margaret D.', tel: '9988676', notes: ''},
-        {name: 'Rubens Hojj', tel: '+55112443', notes: ''},
+    // TODO LOAD
+    $scope.entities = [
+        {name: 'Janet Perkins', tel: '1112212', birthday: new Date(1995, 11, 17), notes: 'Next appointment: 12th Dec'},
+        {name: 'Mary Johnson', tel: '4545454', birthday: new Date(2005, 12, 17), notes: ''},
+        {name: 'Peter Carlsson', tel: '658789', birthday: new Date(1988, 1, 7), notes: ''},
+        {name: 'Margaret D.', tel: '9988676', birthday: new Date(1999, 3, 3), notes: ''},
+        {name: 'Rubens Hojj', tel: '+55112443', birthday: new Date(1999, 1, 9), notes: ''},
         {name: 'Yum Kin', tel: '', notes: ''},
-        {name: 'Walter N. Buzz', tel: '7674323', notes: 'Never on time'},
+        {name: 'Walter N. Buzz', tel: '7674323', birthday: new Date(1955, 4, 18), notes: 'Never on time'}
     ];
 
-    $scope.goToPerson = function (person, event) {
+    $scope.showDetails = function (person, $event) {
 
-        var alert = $mdDialog.alert()
-            .title('Attention, ' + $scope.userName)
-            .content('This is an example of how easy dialogs can be!')
-            .ok('Close');
+        var details = {
+            controller: function DialogController($scope, $mdDialog) {
+                $scope.entity = angular.copy(person);
 
-        $mdDialog.show(alert);
+                $scope.disabled = true;
 
+                $scope.toggleEditDetails = function () {
+                    $scope.disabled = !$scope.disabled;
+                };
+
+                $scope.clean = function () {
+                    $scope.entity = angular.copy(person);
+                };
+
+                $scope.save = function () {
+                    // TODO TOAST!
+                    person = angular.copy($scope.entity);
+                };
+
+                $scope.close = function (answer) {
+                    $mdDialog.hide(answer);
+                };
+            },
+            template: fs.readFileSync(__dirname + '/patient-details.html'),
+            targetEvent: $event
+        };
+
+        // TODO FIX
+        $mdDialog.show(details)
+            .then(function (answer) {
+                $scope.alert = 'SAVE TOAST' + answer;
+            }, function () {
+                $scope.alert = 'CANCEL!!';
+            });
     };
+
 
 };
